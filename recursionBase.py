fd1b9d891dff84026ffa313473ded2b0eea7c977
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-09-01 11:57:11 -0500

    Update recursionBase.py

--- recursionBase.py
+++ recursionBase.py
@@ -1,6 +1,6 @@
 def is_power_of(number, base):
   # Base case: when number is smaller than base.
-  number = number/base
+  number = number / base
   if number < base:
     # If number is equal to 1, it's a power (base**0).
     return False
