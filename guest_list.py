dbe217d19565c99380d9de5b6600ab33f8215cc3
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-09-07 09:18:02 -0500

    Update guest_list.py

--- guest_list.py
+++ guest_list.py
@@ -1,6 +1,6 @@
 def guest_list(guests):
 	for name, age, profession in guests:
-		print("{} is {} years old and works as {}.".format(name, age, profession))
+		print("{} is {} years old and works as {}".format(name, age, profession))
 
 guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
 
