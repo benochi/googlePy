517a32280582c9a91075420ee0a462f7b5ca6253
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-09-27 17:33:12 -0500

    Update changeJane.py

--- week6/changeJane.py
+++ week6/changeJane.py
@@ -7,7 +7,7 @@ path='/home/student-03-28e14b56f865'
 for line in f.readlines():
   old_name = line.strip()
   new_name = old_name.replace("jane", "jdoe")
-subprocess.run(["mv", path+old_name, path+ new_name])
+subprocess.run(["mv", path+old_name, path+new_name])
 f.close()
 
 
