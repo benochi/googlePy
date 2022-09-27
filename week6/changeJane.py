ca6936ee828b06b9a8c78fd77815e380534cb4fc
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-09-27 17:30:54 -0500

    Create changeJane.py

---
+++ week6/changeJane.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+
+f = open(sys.argv[1], "r")
+path='/home/student-03-28e14b56f865'
+for line in f.readlines():
+  old_name = line.strip()
+  new_name = old_name.replace("jane", "jdoe")
+subprocess.run(["mv", path+old_name, path+ new_name])
+f.close()
+
+
+
