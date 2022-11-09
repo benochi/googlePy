7ddef1dbc23f8eb7f8e41520eee9b602406acd10
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-09 15:43:12 -0600

    added more examples

--- module6/requests.py
+++ module6/requests.py
@@ -4,3 +4,15 @@ print(response.text[:300])
 
 #headers 
 response.request.headers['Accept-Encoding']
+
+#boolen res
+response.ok
+
+#status code
+response.status_code
+
+#ex
+url = 'https://www.google.com'
+response = requests.get(url)
+if not response.ok:
+    raise Exception("GET failed with status code {}".format(response.status_code))
