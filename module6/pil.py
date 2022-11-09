3f81d4035782f3fa549855716adddb7d979acd94
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-09 08:03:11 -0600

    lab 1 solution

--- module6/pil.py
+++ module6/pil.py
@@ -8,3 +8,7 @@ from pil import Image
 #im = Image.open("example.jpg")
 #new_im = im.resize((640,480))
 #new_im.save("example_resized.jpg")
+
+#both
+#im = Image.open("example.jpg")
+#im.rotate(180).resize((640,480)).save("flipped_and_resized.jpg")
