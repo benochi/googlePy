351e433d9495b40fdae5fdbc074943e32490e739
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-09 07:43:49 -0600

    pil stuff

--- module6/pil.py
+++ module6/pil.py
@@ -1,3 +1,10 @@
 from pil import Image
-im = Image.open("bride.jpg")
-im.rotate(45).show()
+##rotate
+#im = Image.open("bride.jpg")
+#im.rotate(45).show()
+#new_im.save("example_rotated.jpg")
+
+##resize
+#im = Image.open("example.jpg")
+#new_im = im.resize((640,480))
+#new_im.save("example_resized.jpg")
