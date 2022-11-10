9fbca0a09c8713fe0da23576aadf6d717af0f8b6
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-10 08:13:31 -0600

    more info on attachments

--- module6/attachments.py
+++ module6/attachments.py
@@ -5,3 +5,10 @@ attachment_filename = os.path.basename(attachment_path)
 import mimetypes
 mime_type, _ = mimetypes.guess_type(attachment_path)
 print(mime_type)
+
+#The EmailMessage type needs a MIME type and subtypes as separate strings, so let's split this up:
+mime_type, mime_subtype = mime_type.split('/', 1)
+print(mime_type)
+#image
+print(mime_subtype)
+#png
