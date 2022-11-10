047f6c149ee70f9a95cceea79df572de3f77da04
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-10 08:08:05 -0600

    Create requests.py

---
+++ module6/requests.py
@@ -0,0 +1,47 @@
+import requests
+response = requests.get('https://www.google.com')
+print(response.text[:300])
+
+#headers 
+response.request.headers['Accept-Encoding']
+
+#boolen res
+response.ok
+
+#status code
+response.status_code
+
+#ex
+url = 'https://www.google.com'
+response = requests.get(url)
+if not response.ok:
+    raise Exception("GET failed with status code {}".format(response.status_code))
+
+response = requests.get(url)
+response.raise_for_status()
+
+#more stuff
+p = {
+      "search": "grey kitten",
+      "max_results": 15
+    }
+response = requests.get("https://example.com/path/to/api", params=p)
+response.request.url
+'https://example.com/path/to/api?search=grey+kitten&max_results=15'
+
+###more
+p = {"description": "white kitten",
+     "name": "Snowball",
+     "age_months": 6}
+response = requests.post("https://example.com/path/to/api", data=p)
+
+#
+response.request.url
+response.request.body
+
+###
+response = requests.post("https://example.com/path/to/api", json=p)
+response.request.url
+'https://example.com/path/to/api'
+response.request.body
+b'{"description": "white kitten", "name": "Snowball", "age_months": 6}'
