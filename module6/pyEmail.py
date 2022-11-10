673a483fc6ec3ee0680e0582fb4266c6df6cb962
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-10 07:40:05 -0600

    comment out errors

--- module6/pyEmail.py
+++ module6/pyEmail.py
@@ -9,9 +9,9 @@ message['From'] = sender
 message['To'] = recipient
 message['Subject'] = 'Greetings from {} to {}!'.format(sender, recipient)
 print(message)
-From: me@example.com
-To: you@example.com
-Subject: Greetings from me@example.com to you@example.com!
+#From: me@example.com
+#To: you@example.com
+#Subject: Greetings from me@example.com to you@example.com!
 
 body = """Hey there! I'm learning to send emails using Python!"""
 message.set_content(body)
