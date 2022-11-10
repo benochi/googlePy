805380d29a5b8010cc998c9ed1f7f12b7098a7e2
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-11-10 08:07:38 -0600

    Create pil.py

---
+++ module6/pil.py
@@ -0,0 +1,14 @@
+from pil import Image
+##rotate
+#im = Image.open("bride.jpg")
+#im.rotate(45).show()
+#new_im.save("example_rotated.jpg")
+
+##resize
+#im = Image.open("example.jpg")
+#new_im = im.resize((640,480))
+#new_im.save("example_resized.jpg")
+
+#both
+#im = Image.open("example.jpg")
+#im.rotate(180).resize((640,480)).save("flipped_and_resized.jpg")
